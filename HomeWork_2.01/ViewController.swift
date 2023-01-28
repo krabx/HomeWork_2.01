eca9fb366d8be46c85bd1e8bd64fcad79c7a60e2
Bogdan Radchenko <inroph@mail.ru>
2023-01-28 12:45:33 +0300

    Initial Commit

---
+++ HomeWork_2.01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork_2.01
+//
+//  Created by Богдан Радченко on 28.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
